d844e0f813066d8a2a0b191fd6f054b9d613c522
eundeang <111877048+eundeang@users.noreply.github.com>
2024-09-08 16:19:43 +0900

    init: TimeTracking class에 @EntityListeners(AuditingEntityListener.class) 추가
    엔티티의 변화를 감지

--- src/main/java/org/example/gc_coffee/entity/TimeTracking.java
+++ src/main/java/org/example/gc_coffee/entity/TimeTracking.java
@@ -1,11 +1,14 @@
 package org.example.gc_coffee.entity;
 
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public abstract class TimeTracking {
     @CreatedDate
     private LocalDateTime createdAt;
