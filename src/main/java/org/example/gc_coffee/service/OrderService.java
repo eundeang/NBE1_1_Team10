1f1b71612910698200a4778f3ba9cd2b735473f0
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-09-09 10:45:42 +0900

    fix: orderService interface
    - email로 order를 가져올 때, order를 List에 저장해서 반환하도록 함

--- src/main/java/org/example/gc_coffee/service/OrderService.java
+++ src/main/java/org/example/gc_coffee/service/OrderService.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public interface OrderService {
 
-    OrderDto getOrderByEmail(String email);
+    List<OrderDto> getOrderByEmail(String email);
 
     List<OrderDto> getAllOrders();
 
