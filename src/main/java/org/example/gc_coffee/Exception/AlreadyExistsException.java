7d62e0056cd100ba237a1e5aa6d0e96c44aaf048
YeChan <skfk286@hanmail.net>
2024-09-12 00:30:27 +0900

    서비스 작업도중 발생할 수 있는 에러 상황에 대한 에러코드, 에러 메시지 관리 클래스 추가 / 불필요 Custom Exception 삭제

--- src/main/java/org/example/gc_coffee/Exception/AlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package org.example.gc_coffee.Exception;
-
-public class AlreadyExistsException extends RuntimeException {
-    public AlreadyExistsException(String message) {
-        super(message);
-    }
-}
