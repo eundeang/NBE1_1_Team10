6967426febf1fe4176f5b52a2327a2a86ae82b0d
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-09-11 16:47:42 +0900

    fix: SuperBuilder
    superBuilder를 사용해서 상속받은 컬럼도 접근

--- src/main/java/org/example/gc_coffee/entity/OrderProduct.java
+++ src/main/java/org/example/gc_coffee/entity/OrderProduct.java
@@ -5,10 +5,11 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 @Entity
 @Getter
-@Builder
+@SuperBuilder
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "order_items")
