55296cf105c813a7cfa099cdd6ad2b9b5209b28b
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-09-11 09:25:18 +0900

    refactor: 클래스명 변경

--- src/main/java/org/example/gc_coffee/controller/OrderController.java
+++ src/main/java/org/example/gc_coffee/controller/OrderController.java
@@ -2,9 +2,8 @@ package org.example.gc_coffee.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.example.gc_coffee.dto.OrderDto;
-import org.example.gc_coffee.dto.response.CommonResponseDto;
+import org.example.gc_coffee.dto.response.ApiResponseDto;
 import org.example.gc_coffee.service.OrderService;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,23 +16,20 @@ public class OrderController {
     private final OrderService orderService;
 
     @GetMapping("/{email}")
-    public ResponseEntity<CommonResponseDto<List<OrderDto>>> getOrderByEmail(@PathVariable(value = "email") String email) {
+    public ApiResponseDto<List<OrderDto>> getOrderByEmail(@PathVariable(value = "email") String email) {
         List<OrderDto> orderDtoList = orderService.getOrderByEmail(email);
-        CommonResponseDto<List<OrderDto>> response = new CommonResponseDto<>("Order retrieved successfully", orderDtoList);
-        return ResponseEntity.ok(response);
+        return new ApiResponseDto<>("Order retrieved successfully", orderDtoList);
     }
 
     @GetMapping("/all")
-    public ResponseEntity<CommonResponseDto<List<OrderDto>>> getAllOrder() {
+    public ApiResponseDto<List<OrderDto>> getAllOrder() {
         List<OrderDto> orders = orderService.getAllOrders();
-        CommonResponseDto<List<OrderDto>> response = new CommonResponseDto<>("Orders retrieved successfully", orders);
-        return ResponseEntity.ok(response);
+        return new ApiResponseDto<>("Orders retrieved successfully", orders);
     }
 
     @PostMapping
-    public ResponseEntity<CommonResponseDto<String>> registerOrder(@RequestBody OrderDto orderDto) {
+    public ApiResponseDto<String> registerOrder(@RequestBody OrderDto orderDto) {
         orderService.registerOrder(orderDto);
-        CommonResponseDto<String> response = new CommonResponseDto<>("Order registered successfully \n당일 오후 2시 이후의 주문은 다음날 배송을 시작합니다.", null);
-        return ResponseEntity.ok(response);
+        return new ApiResponseDto<>(201, "Order registered successfully \n당일 오후 2시 이후의 주문은 다음날 배송을 시작합니다.", null);
     }
 }
