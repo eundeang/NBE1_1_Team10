058a9e111f4a07889473efab0eb844dab05575e1
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-09-11 09:25:34 +0900

    feat: 전역 예외 핸들러 추가

--- src/main/java/org/example/gc_coffee/Exception/GlobalExceptionHandler.java
+++ src/main/java/org/example/gc_coffee/Exception/GlobalExceptionHandler.java
@@ -1,9 +1,8 @@
 package org.example.gc_coffee.Exception;
 
 import jakarta.persistence.EntityNotFoundException;
-import org.example.gc_coffee.dto.response.CommonResponseDto;
+import org.example.gc_coffee.dto.response.ApiResponseDto;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -11,26 +10,23 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity<CommonResponseDto<?>> handleEntityNotFoundException(EntityNotFoundException ex) {
-        CommonResponseDto<?> response = new CommonResponseDto<>(HttpStatus.NOT_FOUND.value(), ex.getMessage(), null);
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+    public ApiResponseDto<?> handleEntityNotFoundException(EntityNotFoundException ex) {
+        return new ApiResponseDto<>(HttpStatus.NOT_FOUND.value(), ex.getMessage(), null);
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<CommonResponseDto<?>> handleIllegalArgumentException(IllegalArgumentException ex) {
-        CommonResponseDto<?> response = new CommonResponseDto<>(HttpStatus.BAD_REQUEST.value(), ex.getMessage(), null);
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+    public ApiResponseDto<?> handleIllegalArgumentException(IllegalArgumentException ex) {
+        return new ApiResponseDto<>(HttpStatus.BAD_REQUEST.value(), ex.getMessage(), null);
     }
 
     @ExceptionHandler(AlreadyExistsException.class)
-    public ResponseEntity<CommonResponseDto<?>> handleProductAlreadyExistsException(AlreadyExistsException ex) {
-        CommonResponseDto<?> response = new CommonResponseDto<>(HttpStatus.CONFLICT.value(), ex.getMessage(), null);
-        return ResponseEntity.status(HttpStatus.CONFLICT).body(response);
+    public ApiResponseDto<?> handleProductAlreadyExistsException(AlreadyExistsException ex) {
+        return new ApiResponseDto<>(HttpStatus.CONFLICT.value(), ex.getMessage(), null);
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<CommonResponseDto<?>> handleAllExceptions(Exception ex) {
-        CommonResponseDto<?> response = new CommonResponseDto<>(HttpStatus.INTERNAL_SERVER_ERROR.value(), "An unexpected error occurred: " + ex.getMessage(), null);
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+    public ApiResponseDto<?> handleAllExceptions(Exception ex) {
+        return new ApiResponseDto<>(HttpStatus.INTERNAL_SERVER_ERROR.value(), "An unexpected error occurred: " + ex.getMessage(), null);
     }
+
 }
